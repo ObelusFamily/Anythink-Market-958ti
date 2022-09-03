7e21d8709364f2d6e30c296c5238cbe490d46a3b
Michal Bartosik <msbartosik@gmail.com>
2022-09-03 23:36:04 +0200

    Add show search on word click

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import Search from "./Search";
 
 const Banner = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const handleClick = () => {
+    setShowSearch(true);
+  };
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get </span>
-          <Search />
+          <span id="get-part">
+            A place to <span onClick={handleClick}>get</span>{" "}
+          </span>
+          {showSearch && <Search />}
           <span> the cool stuff.</span>
         </div>
       </div>
